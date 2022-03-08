85bd1e01e495541cd73ac3aa511d664107a7097f
Pablo Perez <perezparraguezp@gmail.com>
2022-03-08 15:07:46 -0300

    Initial commit

---
+++ Connections/localhost.php
@@ -0,0 +1,9 @@
+<?php
+ 
+$hostname_localhost = "localhost";
+$database_localhost = "gym";
+$username_localhost = "root";
+$password_localhost = "pablo2012";
+
+$localhost = mysql_pconnect($hostname_localhost, $username_localhost, $password_localhost) or trigger_error(mysql_error(),E_USER_ERROR); 
+?>
